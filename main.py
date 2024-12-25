4e14c1360ea06c10db5ce6bfe36e1a4d1c286c33
spawlov <107248409+spawlov@users.noreply.github.com>
2024-12-25 19:14:02 +0800

    Refactoring after the review

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ import requests
 import validators
 
 
-load_dotenv(find_dotenv())
-
-
 def shorten_link(token: str, link: str) -> str:
     url = "https://api.vk.ru/method/utils.getShortLink"
     headers = {"Authorization": f"Bearer {token}"}
@@ -44,30 +41,39 @@ def count_clicks(token: str, link: str) -> str:
         return response.json()["response"]["stats"][0]["views"]
 
 
-def is_shorten_link(link: str) -> bool:
-    return furl(link).host == "vk.cc"
+def is_shorten_link(token: str, link: str) -> bool:
+    url = "https://api.vk.ru/method/utils.checkLink"
+    headers = {"Authorization": f"Bearer {token}"}
+    params: dict[str, Any] = {
+        "url": link,
+        "v": 5.199,
+    }
+    with requests.get(url=url, headers=headers, params=params, timeout=30) as response:
+        response.raise_for_status()
+        if "error" in response.text:
+            raise ValueError(f"Произошла ошибка при проверке по ссылки: {link}")
+    return link != response.json()["response"]["link"]
 
 
-def main(link: str) -> str:
-    token = os.getenv("TOKEN")
-    link = link.strip()
-    if not validators.url(link):
-        return f'Некорректная ссылка: "{link}"'
+def main() -> None:
+    load_dotenv(find_dotenv())
+    token = os.environ["VK_TOKEN"]
+    link = input("Введите ссылку: ").strip()
     try:
-        if not is_shorten_link(link):
-            result_message = f"Сокращенная ссылка: {shorten_link(token, link)}"  # type: ignore
+        if not validators.url(link):
+            raise ValueError(f'Некорректная ссылка: "{link}"')
+        if is_shorten_link(token, link):
+            print(f"По ссылке {link} зафиксировано {count_clicks(token, link)} переход(ов)")  # noqa
         else:
-            result_message = f"По ссылке {link} зафиксировано {count_clicks(token, link)} переход(ов)"  # type: ignore
+            print(f"Сокращенная ссылка: {shorten_link(token, link)}")  # noqa
     except (
         requests.exceptions.HTTPError,
         requests.exceptions.ConnectionError,
         requests.exceptions.ConnectTimeout,
         ValueError,
     ) as error:
-        return str(error)
-    return result_message
+        print(str(error))  # noqa
 
 
 if __name__ == "__main__":
-    result = main(input("Введите ссылку: "))
-    print(result)  # noqa
+    main()
